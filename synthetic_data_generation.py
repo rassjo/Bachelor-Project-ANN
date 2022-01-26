1042c6b4f482a8644430e21a09ac656827e13bb1
joebinns <joebinns.95@gmail.com>
2022-01-26 15:40:11 +0100

    Remove default script comment

--- synthetic_data_generation.py
+++ synthetic_data_generation.py
@@ -1,22 +1,6 @@
 """Synthetic Data Generator
 
-This script allows the user to print to the console all columns in the
-spreadsheet. It is assumed that the first row of the spreadsheet is the
-location of the columns.
-
-This script contains various procedures of data generation.
-
-This tool accepts comma separated value files (.csv) as well as excel
-(.xls, .xlsx) files.
-
-This script requires that `pandas` be installed within the Python
-environment you are running this script in.
-
-This file can also be imported as a module and contains the following
-functions:
-
-    * get_spreadsheet_cols - returns the column headers of the file
-    * main - the main function of the script
+This script contains various procedures for data generation.
 """
 
 import numpy as np
