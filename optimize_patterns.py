b37ea2f8d047fdfea8a39e501f94d1415356a62f
Oskise <68348721+Oskise@users.noreply.github.com>
2022-03-15 17:11:50 +0100

    Ability to update existing text files
    Some new features:
    - If you run the same hyperparameters + seed it no longer throws an exception, rather it checks if you're trying a new number of patterns and runs the code for only those.
    - If you don't supply a seed when running the code, it selects a random fixed seed between 100 and 999.
    - When training the model you can now set two flags, save_val and history_plot, which determine if the validation loss should be saved every epoch or just the first and last and if the plot of loss over epochs should be displayed.
    - Some clean-up in optimize_patterns.py, mainly removing redundant comments.
    - Added toggle for the loss/accuracy vs. lambda plots.

--- optimize_patterns.py
+++ optimize_patterns.py
@@ -4,16 +4,25 @@ import activation_functions as act
 import synthetic_data_generation as sdg
 import classification_statistics as cs
 import matplotlib.pyplot as plt
-from id_generator import best_hash
 import sys
 import warnings
+import random as rand
+from id_generator import best_hash
 
 def write_hyperparameters(name,hp,identifier,seed):
-    # If the document already exists, then do nothing.
+    # In case we are running with the same settings, we append the values for
+    # the hyperparameter we have already tried so we don't redo them later
+    tried_values = []
     try:
         open(name, "x")
     except:
-        raise Exception(f"Results from a run with these settings already exists! Please delete {name}!")
+        with open(name, 'r') as data:
+            for line in data:
+                if line[0] != '$':
+                    continue
+                values = line.split(';')
+                tried_values.append(float(values[1][1:-1]))
+        return tried_values
     
     # Overwrite the empty file with the hyperparameters
     with open(name, "w") as f:
@@ -22,36 +31,17 @@ def write_hyperparameters(name,hp,identifier,seed):
         print(f"ID: {identifier}\n", file=f)
         print("Below are the results!\n", file=f)
         print("Best Lambda ; Number of training patterns\n", file=f)
-
-def check_results(model, show=False):
-    loss = 0
-    N = len(trn[0])
-    for n in range(0,N):
-        model.feed_forward(trn[0][n])
-        #print("Pattern", n ," in: ", trn[0][n])
-        if show:
-            print("Pattern", n ,"out:", model.layers[-1].output)
-            print("Pattern", n ,"targ:", trn[1][n])
-        loss += ann.Error(model.layers[-1].output, trn[1][n])
-    return loss/N
-
-def check_layers(model):
-    weights = [layer.weights for layer in model.layers]
-    biases = [layer.biases for layer in model.layers]
-    print("Weights:", weights)
-    print("Biases:", biases)
+    return tried_values
 
 #For lambda
-number_to_try = 5
-
-#For patterns
-numPatterns = 4 #how many (extra) times we make a new number of patterns
-
-#For the model
-minibatchsize = 0 #0 if we don't want to use minibatches
-
+number_to_try = 3
 lambda_x = [0.00001*10**(int(0.25*i))*2**(i%4) for i in range(0,number_to_try)]
 
+#For patterns
+numPatterns = 5 #how many (extra) times we make a new number of patterns
+extra_patterns = [4*10**(int(0.25*i))*2**(i%4) for i in range(0,numPatterns+1)]
+
+#Values for all hyperparameters
 hp = {'lrn_rate': 0.1,
       'epochs': 10,
       'lambdas': lambda_x,
@@ -59,62 +49,72 @@ hp = {'lrn_rate': 0.1,
       'hidden': 15,
       'dataset': 'lagom'}
 
-# For inputting seed from the command line. (i.e. use ```nohup nice n -19 python3 optimize_patterns.py seed &```, where seed is an integer).
-seed = -1
+#Toggle which plots you want to see
+lambda_loss_plot = False ; lambda_acc_plot = False
+
+# For inputting seed from the command line.
+#(i.e. use ```nohup nice n -19 python3 optimize_patterns.py seed &```,
+#where seed is an integer).
+
 try:
   seed = int(sys.argv[1])
 except TypeError:
   warnings.warn("Seed input was not an integer. Using random seed instead.")
+  seed = rand.randint(100,999)
 except:
   warnings.warn("Seed input was missing. Using random seed instead.")
+  seed = rand.randint(100,999)
 
 #For hyperparameters in .txt
-
 hyperparameter_string = str(list(hp.values()))
 identifier = best_hash(hyperparameter_string)
 uniqueFileName = "patterns_lambda_" + str(identifier) + "_" + str(seed) + ".txt"
 
-write_hyperparameters(uniqueFileName,hp,identifier,seed)
+tried_values = write_hyperparameters(uniqueFileName,hp,identifier,seed)
 
+#This is for the final best lambda vs number of pattern plot
 nPatterns = []
 bestLambd = []
 
-if minibatchsize:
-    raise Exception('Turn off stochastic gradient descent!')
+for number in extra_patterns: #range for the numbers of patterns
+    # Create random number generators:
+    # seed == -1 for random rng, seed >= 0 for fixed rng
+    # (seed should be integer)
+    # Reset the RNG seed
+    data_seed = seed
 
-for i in range(0,numPatterns+1): #range for the numbers of patterns
+    def generate_rng(seed):
+        # seed should be an integer
+        # -1 is for random rng, integer >= 0 for fixed
+        if (seed != -1):
+            return np.random.default_rng(data_seed)
+        else:
+            raise Exception('Please set a fixed seed, otherwise results will not be reproducable!')
+            
+    data_rng = generate_rng(data_seed)
+
+    # Import data
+    trn, val = sdg.generate_datasets(hp["dataset"],
+                                 extra = number,
+                                 val_mul = hp["val_mul"],
+                                 try_plot = False,
+                                 rng = data_rng)
+    input_dim = len(trn[0][0]) #Get the input dimension from the training data
+    
+    #If the current number of patterns has already been tried with the same
+    #settings, skip and move on to the next one
+    if len(trn[0]) in tried_values:
+        continue
+    
     trainy = []
     valy = []
     lambd_to_cms = {}
-    extra_patterns = 4*10**(int(0.25*i))*2**(i%4) #patterns = constx10^(int(0.25xtrial))x2^(trial%4)
-    
-    for lambd in lambda_x: #number of lambdas we want to test for each number of hidden nodes
-        #------------------------------------------------------------------------------
-        # Create random number generators:
-        # seed == -1 for random rng, seed >= 0 for fixed rng (seed should be integer)
-        data_seed = seed
+
+    for lambd in lambda_x:
+        #Reset the seed
         ann_seed = data_seed
-    
-        def generate_rng(seed):
-            # seed should be an integer
-            # -1 is for random rng, integer >= 0 for fixed
-            if (seed != -1):
-                return np.random.default_rng(data_seed)
-            else:
-                raise Exception(f'Please set a fixed seed, otherwise results will not be reproducable!')
-                
-        data_rng = generate_rng(data_seed)
         ann_rng = generate_rng(ann_seed)
-    
-        #------------------------------------------------------------------------------
-        # Import data
-        trn, val = sdg.generate_datasets(hp["dataset"],
-                                     extra = extra_patterns,
-                                     val_mul = hp["val_mul"],
-                                     try_plot = False,
-                                     rng = data_rng)
-        input_dim = len(trn[0][0]) #Get the input dimension from the training data
-    
+        
         x_trn = trn[0]
         d_trn = trn[1]
     
@@ -125,71 +125,51 @@ for i in range(0,numPatterns+1): #range for the numbers of patterns
         #Recipe for defining a layer: [number of nodes, activation function, L2]
         layer_defines = [[hp["hidden"], act.tanh, lambd],
                          [1, act.sig, lambd]]
-        # Save as string for .txt.
-        # This is a disgusting way of getting the string... but it wasn't playing nice otherwise.
+       
+        #Create the model!
         test = ann.Model(input_dim, layer_defines, ann_rng)
-    
-        #Check results
-        answer1 = check_results(test, False)
-    
-        outputs = test.feed_all_patterns(x_trn) # Collect the outputs for all the inputs
-        outputs = test.feed_all_patterns(x_val) # Collect the outputs for all the inputs
-    
-        # plt.show()
-        # plt.clf()
-    
-        test.train(trn,val,hp["lrn_rate"],hp["epochs"],minibatchsize) #training, validation, lrn_rate, epochs, minibatchsize=0
-    
-        #Check results again
-        answer2 = check_results(test, False)
-    
+        
+        #Train the model!
+        test.train(trn,val,hp["lrn_rate"],hp["epochs"]) #training, validation, lrn_rate, epochs, minibatchsize=0
+        
+        #Stuff for getting the confusion matrix, I think /Oskar
         outputs = test.feed_all_patterns(x_trn) #Collect the outputs for all the inputs
         cm_trn = cs.construct_confusion_matrix(outputs, d_trn)
     
         outputs = test.feed_all_patterns(x_val) #Collect the outputs for all the inputs
         cm_val = cs.construct_confusion_matrix(outputs, d_val)
     
-        # plt.show()
-        # plt.clf()
-    
-        validation = test.history['val'][-1] #Loss for validation
-    
-        # Display losses
-        # print("Lambda", lambd)
-        # print("Loss before training", answer1)
-        # print("Loss after training", answer2)
-        # print("Validation loss", test.history['val'][-1])
-        # print("")
+        #For the loss vs lambda plot
+        trainy.append(test.history['trn'][-1]) #Loss for validation
+        valy.append(test.history['val'][-1]) #Loss for training
         
-        trainy.append(answer2)
-        valy.append(validation)
-        
+        #Not sure what this is doing????  /Oskar
         lambd_to_cms[lambd] = {"trn": cm_trn, "val": cm_val}
         
 
-    #Appending the number of patterns nodes and best lambda for that number of nodes, to be plotted later
+    #Appending the number of patterns nodes and best lambda for that number of
+    #nodes, to be plotted later
     nPatterns.append(len(trn[0]))
-    
-    #I'm appending the lambda that gives the lowest validation-, and not training-, loss, as that would be zero
     bestLambd.append(lambda_x[valy.index(min(valy))]) 
     
-    # Write the "best lambda ; number of patterns" pairs
-    # In the case of the test crashing, we should adapt the testing loop to start off from where the
-    # previous highest number of patterns was
+    #Write the "best lambda ; number of patterns" pairs
+    #In the case of the test crashing, we should adapt the testing loop to
+    #start off from where the previous highest number of patterns was
     with open(uniqueFileName, "a") as f:
         f.write("$ " + str(lambda_x[valy.index(min(valy))]) + " ; " + str(len(trn[0])) + "\n")
     
+    if lambda_loss_plot:
     #The loss for each lambda is plotted once for every new number of patterns
-    # plt.figure()
-    # plt.plot(lambda_x, trainy, 'go', label='error after train vs lambd')
-    # plt.plot(lambda_x, valy, 'bo', label='Validation error vs lambd')
-    # plt.xlabel('lambdas')
-    # plt.ylabel('Error')
-    # plt.title(f'Error over lambdas for {extra_patterns} extra patterns')
-    # plt.legend()
-    # plt.savefig(f'error_lambda_plot_{extra_patterns}_extra_patterns.png')
-    # plt.show()
-    # plt.clf()
+        plt.figure()
+        plt.plot(lambda_x, trainy, 'go', label='error after train vs lambd')
+        plt.plot(lambda_x, valy, 'bo', label='Validation error vs lambd')
+        plt.xlabel('lambdas')
+        plt.ylabel('Error')
+        plt.title(f'Error over lambdas for {extra_patterns} extra patterns')
+        plt.legend()
+        plt.savefig(f'error_lambda_plot_{extra_patterns}_extra_patterns.png')
+        plt.show()
+        plt.clf()
     
     # Construct a list of training and validation accuracies from the lambd_to_cms dictionary
     acc_trn = []
@@ -199,26 +179,17 @@ for i in range(0,numPatterns+1): #range for the numbers of patterns
         acc_trn.append(stats_trn[0]["advanced"]["acc"])
         stats_val = cs.calculate_stats(lambd_to_cms[lambd]["val"])
         acc_val.append(stats_val[0]["advanced"]["acc"])
-        
-    # Plot the accuracy over lambda plot
-    # plt.figure()
-    # plt.plot(lambda_x, acc_trn, "go", label="Training")
-    # plt.plot(lambda_x, acc_val, "bo", label="Validation")
-    # plt.xlabel("$\lambda$")
-    # plt.ylabel("Accuracy")
-    # plt.title("Accuracy over " + "$\lambda$" + " for " + str(extra_patterns) + " extra patterns")
-    # plt.legend()
-    # plt.savefig(f'accuracy_lambda_plot_{extra_patterns}_extra_patterns.png')
-    # plt.show()
-    # plt.clf()
+    
+    if lambda_acc_plot:
+        # Plot the accuracy over lambda plot
+        plt.figure()
+        plt.plot(lambda_x, acc_trn, "go", label="Training")
+        plt.plot(lambda_x, acc_val, "bo", label="Validation")
+        plt.xlabel("$\lambda$")
+        plt.ylabel("Accuracy")
+        plt.title("Accuracy over " + "$\lambda$" + " for " + str(extra_patterns) + " extra patterns")
+        plt.legend()
+        plt.savefig(f'accuracy_lambda_plot_{extra_patterns}_extra_patterns.png')
+        plt.show()
+        plt.clf()
 
-#The best lambda for each number of patterns is plotted
-# plt.figure()
-# plt.errorbar(nPatterns, bestLambd, yerr=[0.1,0.2,0.3,0.4,0.5], fmt = 'o', color='r', ecolor='b', capsize=5, label='best lambdas vs # patterns')
-# plt.xlabel('# patterns')
-# plt.ylabel('best lambdas')
-# plt.title('best lambdas vs # patterns')
-# plt.legend()
-# plt.savefig('patterns_lambda_plot_' + str(seed) +'.png')
-# plt.show()
-# plt.clf()
