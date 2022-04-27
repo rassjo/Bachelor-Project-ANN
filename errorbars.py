17791a2ee29ba0267237e9f78b48a030f9a10126
Rasmus Sjöö <58602536+rassjo@users.noreply.github.com>
2022-04-27 14:13:33 +0200

    Update errorbars.py

--- errorbars.py
+++ errorbars.py
@@ -29,9 +29,10 @@ def meancalc(x):
 
 all_lambda = {}
 
-# Find the data files
-identifier = '2ce8e323d822'
-seeds = [10,20,30,40,50,60,70,80,90,100]
+identifier = '215b9a39a923'
+seeds = [5,6,7,8,9,10,
+        12,13,14,15,16,17,18,19,20,21,22,23,24,25,
+        100]
 
 # Extract lambda values from data files and append the into lists
 for seed in seeds:
